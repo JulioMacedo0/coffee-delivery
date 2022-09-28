fb225197a34d9aa062f3e8cfa3344b2fc04efd0f
Julio Macedo <juliocesar1076@gmail.com>
2022-09-28 11:45:41 -0300

    fix: add global styles and themeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-
-
+import { ThemeProvider } from "styled-components";
+import { Header } from "./Components/Header";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
-  
+
   return (
-    <h1>Teste</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle/>
+      <Header/>
+    </ThemeProvider>
   )
 }
