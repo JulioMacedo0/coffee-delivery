97be997e3104d4292359988e577372ab55e79a36
Julio Macedo <juliocesar1076@gmail.com>
2022-09-29 17:31:38 -0300

    fix: add CardCoffe component

--- src/Components/Coffees/index.tsx
+++ src/Components/Coffees/index.tsx
@@ -1,9 +1,21 @@
+import { CardCoffee } from "../CardCoffee";
 import * as S from "./styles";
+
 export const Coffees = () => {
   return (
     <S.Container>
       <S.CoffesContainer>
         <h2>Nossos cafés</h2>
+        <S.GridContainer>
+          <CardCoffee />
+          <CardCoffee />
+          <CardCoffee />
+          <CardCoffee />
+          <CardCoffee />
+          <CardCoffee />
+          <CardCoffee />
+          <CardCoffee />
+        </S.GridContainer>
       </S.CoffesContainer>
     </S.Container>
   );
