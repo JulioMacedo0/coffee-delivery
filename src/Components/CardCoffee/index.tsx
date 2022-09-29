0d2c31bd5c362df48344c3edc5b440d76c1205af
Julio Macedo <juliocesar1076@gmail.com>
2022-09-29 17:28:56 -0300

    fix: create CardCoffee component

---
+++ src/Components/CardCoffee/index.tsx
@@ -0,0 +1,33 @@
+import { Minus, Plus, ShoppingCartSimple } from "phosphor-react";
+import cafe from "../../assets/ExpressoTradicional.svg";
+import * as S from "./styles";
+export const CardCoffee = () => {
+  return (
+    <S.Container>
+      <img src={cafe} alt="" />
+      <div>
+        <S.TasteSpan>tradicional</S.TasteSpan>
+      </div>
+
+      <h3>Expresso Tradicional</h3>
+      <p>O tradicional café feito com água quente e grãos moídos</p>
+      <footer>
+        <S.PricingContainer>
+          <S.CurrencyType>R$ </S.CurrencyType> <S.Price>9,90</S.Price>
+        </S.PricingContainer>
+        <S.CounterItem>
+          <button>
+            <Minus size={14} weight="fill" />
+          </button>
+          1
+          <button>
+            <Plus size={14} weight="fill" />
+          </button>
+        </S.CounterItem>
+        <button>
+          <ShoppingCartSimple size={22} weight="fill" />
+        </button>
+      </footer>
+    </S.Container>
+  );
+};
---
+++ src/Components/CardCoffee/styles.ts
@@ -0,0 +1,141 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  border-top-left-radius: 6px;
+  border-top-right-radius: 32px;
+  border-bottom-left-radius: 32px;
+  border-bottom-right-radius: 6px;
+  display: flex;
+  flex-direction: column;
+  width: 16rem;
+  height: 19.375rem;
+  align-items: center;
+  background-color: ${(props) => props.theme["base-card"]};
+
+  padding: 0px 0.25rem;
+  img {
+    position: relative;
+    top: -20px;
+    width: 7.5rem;
+    height: 7.5rem;
+  }
+
+  > div {
+    margin-bottom: 1rem;
+  }
+
+  h3 {
+    color: ${(props) => props.theme["bate-subtitle"]};
+    font-family: "Baloo 2";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 130%;
+    margin-bottom: 0.5rem;
+  }
+
+  > p {
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 130%;
+    color: ${(props) => props.theme["base-label"]};
+    text-align: center;
+    margin-bottom: 2.0625rem;
+  }
+
+  footer {
+    width: 100%;
+    height: 2.375rem;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    > button {
+      border-radius: 6px;
+      border: none;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 2rem;
+      height: 2rem;
+      background-color: ${(props) => props.theme["purple-darl"]};
+      color: ${(props) => props.theme["white"]};
+      transition: background-color 0.5s;
+      :hover {
+        background-color: ${(props) => props.theme["purple"]};
+      }
+    }
+  }
+`;
+
+export const TasteSpan = styled.span`
+  background-color: ${(props) => props.theme["yellow-light"]};
+  color: ${(props) => props.theme["yellow-dark"]};
+  padding: 0.25rem 0.5rem;
+  border-radius: 8px;
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 0.625rem;
+  line-height: 130%;
+
+  text-transform: uppercase;
+`;
+
+export const PricingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.25rem;
+`;
+
+export const CurrencyType = styled.span`
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 0.875rem;
+  line-height: 1.125rem;
+  text-align: right;
+  color: ${(props) => props.theme["bate-text"]};
+`;
+
+export const Price = styled.span`
+  font-family: "Baloo 2";
+  font-style: normal;
+  font-weight: 800;
+  font-size: 1.5rem;
+  line-height: 1.875rem;
+  text-align: right;
+  color: ${(props) => props.theme["bate-text"]};
+`;
+
+export const CounterItem = styled.span`
+  padding: 8px 12px;
+  border-radius: 6px;
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1rem;
+  line-height: 130%;
+
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme["base-button"]};
+  color: ${(props) => props.theme["base-title"]};
+  gap: 4px;
+
+  button {
+    border: none;
+    background-color: inherit;
+    color: ${(props) => props.theme["purple"]};
+    cursor: pointer;
+
+    :hover {
+      color: ${(props) => props.theme["purple-darl"]};
+    }
+  }
+`;
