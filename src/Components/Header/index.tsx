eec6d80d2d10a9e3b855789811544c705183e60d
Julio Macedo <juliocesar1076@gmail.com>
2022-09-29 10:35:04 -0300

    fix: prettier format

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,21 +1,23 @@
 import { MapPin, ShoppingCart } from "phosphor-react";
-import * as S from './styles';
-import logo from '../../assets/Logo.svg'
+import * as S from "./styles";
+import logo from "../../assets/Logo.svg";
 export const Header = () => {
   return (
     <S.Container>
-   <S.HeaderContainer>
-    <img src={logo} alt="logo" />
+      <S.HeaderContainer>
+        <img src={logo} alt="logo" />
 
-    <div>
-      <S.LocaleDiv>
-      <MapPin size={22} weight="fill" />
-      <p>Tocantinopolis, TO</p>
-      </S.LocaleDiv>
+        <div>
+          <S.LocaleDiv>
+            <MapPin size={22} weight="fill" />
+            <p>Tocantinopolis, TO</p>
+          </S.LocaleDiv>
 
-    <S.ButtonChart> <ShoppingCart size={22} weight="fill" /></S.ButtonChart>
-    </div>
-   </S.HeaderContainer>
-   </S.Container>
-  )
-}
+          <S.ButtonChart onClick={() => console.log("click")}>
+            <ShoppingCart size={22} weight="fill" />
+          </S.ButtonChart>
+        </div>
+      </S.HeaderContainer>
+    </S.Container>
+  );
+};
--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -1,55 +1,57 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-display: flex;
-width: 100%;
-justify-content: center;
-`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+`;
 export const HeaderContainer = styled.header`
-width: 100%;
-max-width: 90rem;
-height: 6.5rem;
-display: flex;
-justify-content: space-between;
-align-items: center;
-padding: 2rem 10rem;
+  width: 100%;
+  max-width: 90rem;
+  height: 6.5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 10rem;
 
-div  {
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 2.375rem;
+  }
+`;
+
+export const ButtonChart = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
+  border-radius: 6px;
+  margin-left: 0.75rem;
+  background-color: ${(props) => props.theme["yellow-light"]};
+  color: ${(props) => props.theme["yellow-dark"]};
   height: 2.375rem;
-}
-`
-
-export const ButtonChart = styled.button`
-display: flex;
-align-items: center;
-justify-content: center;
-border-radius: 6px;
-margin-left: 0.75rem;
-background-color: ${(props) => props.theme['yellow-light']};
-color: ${(props) => props.theme['yellow-dark']};
-height:  2.375rem;
-width: 2.375rem;
-border: none;
-
-`
-
+  width: 2.375rem;
+  border: none;
+  transition: background-color 0.5s;
+  :hover {
+    background-color: ${(props) => props.theme["yellow"]};
+  }
+`;
 
 export const LocaleDiv = styled.div`
-border-radius: 6px;
-padding: 0.5rem;
-color: ${(props) => props.theme['purple-darl']};
-background-color: ${(props) => props.theme['purple-light']};;
-p {
-   font-family: 'Roboto', sans-serif;
-  font-size: 0.875rem;
-  line-height: 1.1375rem;
-  font-weight: 400;
-}
+  border-radius: 6px;
+  padding: 0.5rem;
+  color: ${(props) => props.theme["purple-darl"]};
+  background-color: ${(props) => props.theme["purple-light"]};
+  p {
+    font-family: "Roboto", sans-serif;
+    font-size: 0.875rem;
+    line-height: 1.1375rem;
+    font-weight: 400;
+  }
 
-svg {
-  color: ${(props) => props.theme['purple']};
-}
-`
+  svg {
+    color: ${(props) => props.theme["purple"]};
+  }
+`;
