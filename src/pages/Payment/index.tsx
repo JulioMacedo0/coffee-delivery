9d49e915041d3860975f9ad4c6d6b0fff651e5b2
Julio Macedo <juliocesar1076@gmail.com>
2022-09-30 12:06:05 -0300

    fix: create payment page

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -1,5 +1,58 @@
+import { CurrencyDollar, MapPinLine } from "phosphor-react";
 import * as S from "./styles";
 
 export const Payment = () => {
-  return <S.teste>pagamento</S.teste>;
+  return (
+    <S.Container>
+      <S.ContentContainer>
+        <div>
+          <S.Title>Complete seu pedido</S.Title>
+          <S.AddressContainer>
+            <S.FormContainer>
+              <header>
+                <div>
+                  <MapPinLine size={22} />
+                  <S.Span>Endereço de Entrega</S.Span>
+                </div>
+
+                <S.P>Informe o endereço onde deseja receber seu pedido</S.P>
+              </header>
+
+              <form action="">
+                <S.CepInput placeholder="CEP" />
+                <S.StreetInput placeholder="Rua" />
+                <S.InputContainer>
+                  <S.NumberInput placeholder="Número" />
+                  <S.ComplementInput placeholder="Complemento" />
+                </S.InputContainer>
+                <S.InputContainer>
+                  <S.DistrictInput placeholder="Bairro" />
+                  <S.CityInput placeholder="Cidade" />
+                  <S.UfInput placeholder="UF" />
+                </S.InputContainer>
+              </form>
+            </S.FormContainer>
+          </S.AddressContainer>
+
+          <S.ChosePaymentContainer>
+            <header>
+              <div>
+                <CurrencyDollar size={22} />
+                <S.Span>Pagamento</S.Span>
+              </div>
+
+              <S.P>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </S.P>
+            </header>
+          </S.ChosePaymentContainer>
+        </div>
+
+        <div>
+          <S.Title>Cafés selecionados</S.Title>
+          <S.ResumePricingContent>test</S.ResumePricingContent>
+        </div>
+      </S.ContentContainer>
+    </S.Container>
+  );
 };
--- src/pages/Payment/styles.ts
+++ src/pages/Payment/styles.ts
@@ -1,5 +1,150 @@
 import styled from "styled-components";
 
-export const teste = styled.h1`
-  background-color: red;
+export const Container = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+`;
+
+export const ContentContainer = styled.div`
+  width: 100%;
+  max-width: 90rem;
+  height: 6.5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding: 2rem 10rem;
+`;
+
+export const Title = styled.h2`
+  font-family: "Baloo 2";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1.125rem;
+  line-height: 130%;
+  color: ${(props) => props.theme["bate-subtitle"]};
+  margin-bottom: 0.75rem;
+`;
+
+export const AddressContainer = styled.div`
+  border-radius: 6px;
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+  width: 40rem;
+  height: 23.25rem;
+  margin-bottom: 0.75rem;
+`;
+
+export const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  background-color: ${(props) => props.theme["base-card"]};
+  border-radius: 6px;
+  padding: 2.5rem;
+  gap: 2rem;
+  height: 23.25rem;
+
+  header {
+    div {
+      display: flex;
+      align-items: center;
+    }
+    svg {
+      color: ${(props) => props.theme["yellow-dark"]};
+    }
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  gap: 0.75rem;
+`;
+
+export const BaseInput = styled.input`
+  padding: 0.75rem;
+  height: 2.625rem;
+  outline: none;
+  border: 1px solid ${(props) => props.theme["base-button"]};
+  background-color: ${(props) => props.theme["base-input"]};
+  border-radius: 4px;
+`;
+
+export const CepInput = styled(BaseInput)`
+  width: 12.5rem;
+`;
+
+export const StreetInput = styled(BaseInput)``;
+export const NumberInput = styled(BaseInput)`
+  width: 12.5rem;
+`;
+export const ComplementInput = styled(BaseInput)`
+  width: 100%;
+`;
+export const DistrictInput = styled(BaseInput)`
+  width: 12.5rem;
+`;
+export const CityInput = styled(BaseInput)`
+  width: 17.25rem;
+`;
+export const UfInput = styled(BaseInput)`
+  width: 3.75rem;
+`;
+
+export const ChosePaymentContainer = styled.div`
+  background-color: ${(props) => props.theme["base-card"]};
+  border-radius: 6px;
+  padding: 2.5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+  width: 100%;
+  height: 19.9375rem;
+
+  header {
+    div {
+      display: flex;
+      align-items: center;
+    }
+    svg {
+      color: ${(props) => props.theme["purple"]};
+    }
+  }
+`;
+
+export const Span = styled.span`
+  margin-left: 0.5rem;
+  color: ${(props) => props.theme["bate-subtitle"]};
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1rem;
+  line-height: 130%;
+`;
+
+export const P = styled.p`
+  margin-left: 1.875rem;
+  color: ${(props) => props.theme["bate-text"]};
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 0.875rem;
+  line-height: 130%;
+`;
+
+export const ResumePricingContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  background-color: ${(props) => props.theme["base-card"]};
+  padding: 2.5rem;
+  width: 28rem;
+  height: 31.125rem;
 `;
