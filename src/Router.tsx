07dcc2884782ef8bdc973ee8885b7213304cf6af
Julio Macedo <juliocesar1076@gmail.com>
2022-10-01 14:29:28 -0300

    fix: add new route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,15 @@
 import { Routes, Route } from "react-router-dom";
-import { Header } from "./Components/Header";
 
 import { Home } from "./pages/Home/Home";
 import { Payment } from "./pages/Payment";
+import { PaymentConfirmed } from "./pages/PaymentConfirmed";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/payment" element={<Payment />} />
+      <Route path="/paymentConfirm" element={<PaymentConfirmed />} />
     </Routes>
   );
 }
