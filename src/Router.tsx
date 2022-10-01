5f7d443388692380398f3c44a3946ce53afed6a8
Julio Macedo <juliocesar1076@gmail.com>
2022-10-01 18:57:02 -0300

    fix: add 404 route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 
 import { Home } from "./pages/Home/Home";
+import { NotFound } from "./pages/NotFound";
 import { Payment } from "./pages/Payment";
 import { PaymentConfirmed } from "./pages/PaymentConfirmed";
 
 export function Router() {
   return (
     <Routes>
+      <Route path="*" element={<NotFound />} />
       <Route path="/" element={<Home />} />
       <Route path="/payment" element={<Payment />} />
       <Route path="/paymentConfirmed" element={<PaymentConfirmed />} />
