566e767b15bc06851a9ce27e1e8dff8ebeaf30f5
Julio Macedo <juliocesar1076@gmail.com>
2022-10-03 18:57:13 -0300

    fix: add props in CardCoffee

--- src/pages/Home/Components/CardCoffee/index.tsx
+++ src/pages/Home/Components/CardCoffee/index.tsx
@@ -2,21 +2,37 @@ import { Minus, Plus, ShoppingCartSimple } from "phosphor-react";
 import { NavLink } from "react-router-dom";
 import cafe from "../../../../assets/ExpressoTradicional.svg";
 import * as S from "./styles";
-export const CardCoffee = () => {
+console.log(cafe);
+interface CardCoffeProps {
+  img: string;
+  tags: [];
+  name: string;
+  description: string;
+  price: number;
+}
+
+export const CardCoffee = ({
+  img,
+  tags,
+  name,
+  description,
+  price,
+}: CardCoffeProps) => {
   return (
     <S.Container>
-      <img src={cafe} alt="" />
+      <img src={img} alt="" />
       <S.TasteContainer>
-        <S.TasteSpan>tradicional</S.TasteSpan>
-        <S.TasteSpan>tradicional</S.TasteSpan>
+        {tags.map((tag) => (
+          <S.TasteSpan>{tag}</S.TasteSpan>
+        ))}
       </S.TasteContainer>
 
-      <h3>Expresso Tradicional</h3>
-      <p>O tradicional café feito com água quente e grãos moídos</p>
+      <h3>{name}</h3>
+      <p>{description}</p>
 
       <footer>
         <S.PricingContainer>
-          <S.CurrencyType>R$ </S.CurrencyType> <S.Price>9,90</S.Price>
+          <S.CurrencyType>R$ </S.CurrencyType> <S.Price>{price}</S.Price>
         </S.PricingContainer>
         <S.CounterItem>
           <button>
--- src/pages/Home/Components/Content/index.tsx
+++ src/pages/Home/Components/Content/index.tsx
@@ -1,20 +1,43 @@
+import { useEffect, useState } from "react";
 import { CardCoffee } from "../CardCoffee";
 import * as S from "./styles";
+import { api } from "../../../../API/api";
+
+interface CoffesProps {
+  description: string;
+  id: number;
+  name: string;
+  photo: string;
+  price: number;
+  tags: [];
+}
 
 export const Content = () => {
+  const [Coffes, SetCoffes] = useState<CoffesProps[]>([]);
+
+  useEffect(() => {
+    api.get("/products").then((response) => {
+      SetCoffes(response.data);
+    });
+  }, []);
+
   return (
     <S.Container>
       <S.CoffesContainer>
         <h2>Nossos cafés</h2>
         <S.GridContainer>
-          <CardCoffee />
-          <CardCoffee />
-          <CardCoffee />
-          <CardCoffee />
-          <CardCoffee />
-          <CardCoffee />
-          <CardCoffee />
-          <CardCoffee />
+          {Coffes.map((coffe) => {
+            return (
+              <CardCoffee
+                key={coffe.id}
+                description={coffe.description}
+                img={coffe.photo}
+                name={coffe.name}
+                price={coffe.price}
+                tags={coffe.tags}
+              />
+            );
+          })}
         </S.GridContainer>
       </S.CoffesContainer>
     </S.Container>
