368e9d4647eb51ce8bdd632182f7b7f1e1cfa057
Julio Macedo <juliocesar1076@gmail.com>
2022-10-04 12:09:29 -0300

    fix: function add product

--- src/pages/Home/Components/CardCoffee/index.tsx
+++ src/pages/Home/Components/CardCoffee/index.tsx
@@ -1,9 +1,11 @@
 import { Minus, Plus, ShoppingCartSimple } from "phosphor-react";
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 import * as S from "./styles";
 
 interface CardCoffeProps {
+  id: number;
   img: string;
   tags: [];
   name: string;
@@ -12,12 +14,66 @@ interface CardCoffeProps {
 }
 
 export const CardCoffee = ({
+  id,
   img,
   tags,
   name,
   description,
   price,
 }: CardCoffeProps) => {
+  interface ChartItemType {
+    id: number;
+    name: string;
+    img: string;
+    price: string;
+    amount: number;
+  }
+
+  const [itemsChart, setItemsChart] = useState<ChartItemType[]>([]);
+
+  useEffect(() => {
+    const StotredItems = localStorage.getItem(
+      "@coffe-delivery:chart-items1.0.0"
+    );
+
+    if (StotredItems) {
+      setItemsChart(JSON.parse(StotredItems));
+    }
+  }, []);
+
+  const AlreadyExists = itemsChart.length
+    ? itemsChart.map((item) => {
+        if (item.id == id) {
+          return true;
+        } else {
+          return false;
+        }
+      })
+    : false;
+
+  function HandlleAddProduct() {
+    if (AlreadyExists) {
+      console.log("valor ja existe");
+    } else {
+      const newItems = [...itemsChart, { id, name, img, price, amount: 1 }];
+      console.log(newItems);
+      setItemsChart(newItems);
+    }
+    console.log(itemsChart);
+    localStorage.setItem(
+      "@coffe-delivery:chart-items1.0.0",
+      JSON.stringify(itemsChart)
+    );
+  }
+
+  const amountItem = itemsChart.length
+    ? itemsChart.map((item) => {
+        if (item.id == id) {
+          return item.amount;
+        }
+      })
+    : 0;
+
   return (
     <S.Container>
       <img src={img} alt="" />
@@ -38,8 +94,8 @@ export const CardCoffee = ({
           <button>
             <Minus size={15} weight="fill" />
           </button>
-          1
-          <button>
+          {amountItem}
+          <button onClick={() => HandlleAddProduct()}>
             <Plus size={15} weight="fill" />
           </button>
         </S.CounterItem>
