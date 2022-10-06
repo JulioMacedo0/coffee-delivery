2a50d2774ac2cb6bb6f0f034f1aad8174ad7ab25
Julio Macedo <juliocesar1076@gmail.com>
2022-10-06 10:58:02 -0300

    fix: add props

--- src/pages/Payment/Components/CardPayment/index.tsx
+++ src/pages/Payment/Components/CardPayment/index.tsx
@@ -1,12 +1,28 @@
+import { useContext } from "react";
+import { ChartItemsContext } from "../../../../context/ChartItemsContext";
 import * as S from "./styles";
 
 interface CardPaymentProps {
   PaymentMethod: string;
   icon: React.ReactNode;
+  paymentSelected: string;
+  payment: string;
 }
-export const CardPayment = ({ PaymentMethod, icon }: CardPaymentProps) => {
+export const CardPayment = ({
+  PaymentMethod,
+  icon,
+  paymentSelected,
+  payment,
+}: CardPaymentProps) => {
+  const { handleChangePaymentSlected } = useContext(ChartItemsContext);
+
   return (
-    <S.Container>
+    <S.Container
+      PaymentMethod={PaymentMethod}
+      paymentSelected={paymentSelected}
+      type="button"
+      onClick={() => handleChangePaymentSlected(payment)}
+    >
       <span>{icon}</span>
       <p>{PaymentMethod}</p>
     </S.Container>
