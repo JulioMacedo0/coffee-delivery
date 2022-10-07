c625eef80e670de1247af8e095ed916ba09d988a
Julio Macedo <juliocesar1076@gmail.com>
2022-10-07 11:22:08 -0300

    fix: add input state

--- src/context/ChartItemsContext.tsx
+++ src/context/ChartItemsContext.tsx
@@ -11,6 +11,37 @@ interface ChartItemType {
   amount: number;
 }
 
+interface FromDataType {
+  cep: {
+    data: string;
+    isValid: boolean;
+  };
+  street: {
+    data: string;
+    isValid: boolean;
+  };
+  houseNumber: {
+    data: string;
+    isValid: boolean;
+  };
+  complement: {
+    data: string;
+    isValid: boolean;
+  };
+  district: {
+    data: string;
+    isValid: boolean;
+  };
+  city: {
+    data: string;
+    isValid: boolean;
+  };
+  state: {
+    data: string;
+    isValid: boolean;
+  };
+}
+
 interface ChartItemsContextType {
   itemsChart: ChartItemType[];
   handleAddProduct: (
@@ -19,16 +50,24 @@ interface ChartItemsContextType {
     img: string,
     price: string
   ) => void;
+  handleFormValidation: () => void;
   handleDecreaseProduct: (id: number) => void;
   handleDeleteProduct: (id: number) => void;
+  onChangeSetCep: (cep: string) => void;
   onChangeSetScreet: (street: string) => void;
   onChangeSetHouseNumber: (houseNumber: string) => void;
+  onChangeSetComplement: (complement: string) => void;
+  onChangeSetDistrict: (district: string) => void;
   onChangeSetCity: (city: string) => void;
   onChangeSetState: (state: string) => void;
   handleChangePaymentSlected: (payment: string) => void;
+  inputData: FromDataType;
   paymentSelected: string;
+  cep: string;
   street: string;
   houseNumber: string;
+  complement: string;
+  district: string;
   city: string;
   state: string;
 }
@@ -39,11 +78,48 @@ export function ChartItemsContextProvider({
   children,
 }: ChartItemsContextProps) {
   const [itemsChart, setItemsChart] = useState<ChartItemType[]>([]);
+  const [cep, setCep] = useState("");
   const [street, setStreet] = useState("");
   const [houseNumber, setHouseNumber] = useState("");
+  const [complement, setComplement] = useState("");
+  const [district, setDistric] = useState("");
   const [city, setCity] = useState("");
   const [state, setState] = useState("");
   const [paymentSelected, setPaymentSeletected] = useState("");
+  const [inputData, setInputData] = useState<FromDataType>({
+    cep: {
+      data: "",
+      isValid: false,
+    },
+    street: {
+      data: "",
+      isValid: false,
+    },
+    houseNumber: {
+      data: "",
+      isValid: false,
+    },
+    complement: {
+      data: "",
+      isValid: false,
+    },
+    district: {
+      data: "",
+      isValid: false,
+    },
+    city: {
+      data: "",
+      isValid: false,
+    },
+    state: {
+      data: "",
+      isValid: false,
+    },
+  });
+
+  function onChangeSetCep(cep: string) {
+    setCep(cep);
+  }
 
   function onChangeSetScreet(street: string) {
     setStreet(street);
@@ -51,6 +127,14 @@ export function ChartItemsContextProvider({
 
   function onChangeSetHouseNumber(houseNumber: string) {
     setHouseNumber(houseNumber);
+  }
+
+  function onChangeSetComplement(complement: string) {
+    setComplement(complement);
+  }
+
+  function onChangeSetDistrict(district: string) {
+    setDistric(district);
   }
 
   function onChangeSetCity(city: string) {
@@ -61,20 +145,44 @@ export function ChartItemsContextProvider({
     setState(state);
   }
 
+  function handleFormValidation() {
+    const objData = {
+      cep: {
+        data: cep,
+        isValid: cep ? true : false,
+      },
+      street: {
+        data: street,
+        isValid: street ? true : false,
+      },
+      houseNumber: {
+        data: houseNumber,
+        isValid: houseNumber ? true : false,
+      },
+      complement: {
+        data: complement,
+        isValid: complement ? true : false,
+      },
+      district: {
+        data: district,
+        isValid: district ? true : false,
+      },
+      city: {
+        data: city,
+        isValid: city ? true : false,
+      },
+      state: {
+        data: state,
+        isValid: state ? true : false,
+      },
+    };
+    setInputData(objData);
+  }
+
   function handleChangePaymentSlected(payment: string) {
     setPaymentSeletected(payment);
     console.log(payment);
   }
-
-  useEffect(() => {
-    const StotredItems = localStorage.getItem(
-      "@coffe-delivery:chart-items1.0.0"
-    );
-
-    if (StotredItems) {
-      setItemsChart(JSON.parse(StotredItems));
-    }
-  }, []);
 
   function handleAddProduct(
     id: number,
@@ -133,23 +241,41 @@ export function ChartItemsContextProvider({
     setItemsChart(newItems);
   }
 
+  useEffect(() => {
+    const StotredItems = localStorage.getItem(
+      "@coffe-delivery:chart-items1.0.0"
+    );
+
+    if (StotredItems) {
+      setItemsChart(JSON.parse(StotredItems));
+    }
+  }, []);
+
   return (
     <ChartItemsContext.Provider
       value={{
-        itemsChart,
+        handleFormValidation,
         handleAddProduct,
         handleDecreaseProduct,
+        handleChangePaymentSlected,
         handleDeleteProduct,
+        onChangeSetCep,
         onChangeSetScreet,
         onChangeSetHouseNumber,
+        onChangeSetComplement,
+        onChangeSetDistrict,
         onChangeSetCity,
         onChangeSetState,
-        handleChangePaymentSlected,
+        inputData,
+        itemsChart,
         city,
         houseNumber,
         state,
         street,
         paymentSelected,
+        cep,
+        complement,
+        district,
       }}
     >
       {children}
